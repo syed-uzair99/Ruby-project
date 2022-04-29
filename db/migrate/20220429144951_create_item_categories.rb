d882b1c4228d4ac033fe5b7815c94456c2dcde93
deepika-prajapati <102571447+deepika-prajapati@users.noreply.github.com>
2022-04-29 20:22:14 +0530

    DB structure change

--- db/migrate/20220429144951_create_item_categories.rb
+++ db/migrate/20220429144951_create_item_categories.rb
@@ -1,7 +1,7 @@
 class CreateItemCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :item_categories do |t|
-      t.integer :C.No
+      t.integer :CategoryNo
       t.integer :CategoryID
       t.string :CategoryName
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_062850) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_144951) do
+  create_table "item_categories", force: :cascade do |t|
+    t.integer "CategoryNo"
+    t.integer "CategoryID"
+    t.string "CategoryName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "item_lists", force: :cascade do |t|
+    t.integer "ItemNo"
+    t.integer "CategoryID"
+    t.string "CategoryName"
+    t.integer "ItemID"
+    t.string "ItemName"
+    t.float "ItemPrice"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sale_details", force: :cascade do |t|
+    t.integer "SaleNo"
+    t.integer "CategoryID"
+    t.string "CategoryName"
+    t.integer "ItemId"
+    t.string "ItemName"
+    t.string "TotalQuantity"
+    t.decimal "ItemPrice"
+    t.decimal "TotalCost"
+    t.datetime "DateofSale"
+    t.string "CustomerName"
+    t.integer "OrderID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stock_details", force: :cascade do |t|
+    t.integer "StockNo"
+    t.integer "CategoryID"
+    t.string "CategoryName"
+    t.integer "ItemId"
+    t.string "ItemName"
+    t.integer "TotalStock"
+    t.integer "SoldStock"
+    t.integer "RemainingStock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "user_roles", force: :cascade do |t|
     t.string "roleName"
     t.datetime "created_at", null: false
