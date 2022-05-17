1078b1ab8864cce1685b4a012a486466a46ecf85
Amrush-hub <102530023+Amrush-hub@users.noreply.github.com>
2022-05-17 15:54:02 +0530

    final

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,30 +1,5 @@
 class AppController < ActionController::Base
     
-    def viewww
-        @uu=User.all
-        @u=@uu.order(name: :asc)
-        @count=User.count
-        @s=SaleDetail.all
-        @st=StockDetail.all
-        @stt=@st.order(ItemName: :asc)
-        @x=SaleDetail.where(CategoryName:"Books")
-        @xx=@x.order(created_at: :desc)
-        @y=SaleDetail.where(CategoryName:"Furniture")
-        @yy=@y.order(created_at: :desc)
-        @z=SaleDetail.where(CategoryName:"Clothes")
-        @zz=@z.order(created_at: :desc)
-        @b=@x.sum(:TotalCost)
-        @f=@y.sum(:TotalCost)
-        @c=@z.sum(:TotalCost)
-        for i in 1..15 do
-            @d=SaleDetail.where(ItemId:i)
-            sum=@d.sum(:TotalQuantity)
-            @dd=StockDetail.where(ItemId:i)
-            t=@dd[0]["TotalStock"]
-            @dd.update(RemainingStock:t-sum, SoldStock:sum)
-        end    
-        render "layouts/viewww"
-    end
 
     def form
         me = current_user.id
@@ -57,5 +32,41 @@ class AppController < ActionController::Base
         @i=SaleDetail.where(SaleNo:me)
         render "layouts/pur"
      end   
+
+     def admin
+        render "layouts/ad"
+     end 
+
+     def min
+        @uu=User.all
+        @u=@uu.order(name: :asc)
+        @count=User.count
+        @s=SaleDetail.all
+        @st=StockDetail.all
+        @stt=@st.order(ItemName: :asc)
+        @x=SaleDetail.where(CategoryName:"Books")
+        @xx=@x.order(created_at: :desc)
+        @y=SaleDetail.where(CategoryName:"Furniture")
+        @yy=@y.order(created_at: :desc)
+        @z=SaleDetail.where(CategoryName:"Clothes")
+        @zz=@z.order(created_at: :desc)
+        @b=@x.sum(:TotalCost)
+        @f=@y.sum(:TotalCost)
+        @c=@z.sum(:TotalCost)
+        for i in 1..15 do
+            @d=SaleDetail.where(ItemId:i)
+            sum=@d.sum(:TotalQuantity)
+            @dd=StockDetail.where(ItemId:i)
+            t=@dd[0]["TotalStock"]
+            @dd.update(RemainingStock:t-sum, SoldStock:sum)
+        end    
+        n=params[:name]
+        p=params[:password]
+     if (n=="Coinbase" && p="coinbase")
+        render "layouts/viewww"  
+     else   
+        render "layouts/ad"
+     end   
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/construct'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'view', to: 'app#viewww'
   post 'app/create', to: 'app#create'
   get 'form', to: 'app#form'
   get 'pur', to: 'app#pur'
+  get 'admin', to: 'app#admin'
   post 'app/new', to: 'app#new'
+  post 'app/min', to: 'app#min'
   # Defines the root path route ("/")
   root "home#index"
 end
